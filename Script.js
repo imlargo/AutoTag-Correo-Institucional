9fe13f1d249f4d819e1641526e1f0d7a9cbaebed
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-08-05 21:03:20 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,108 +1,78 @@
-//Trabajando en mejorar los permisos
 //Creado por @im largo
 
-let addedLabels = []
-let label_name;
+const allLabels = {
 
-let sub_Materias = [
-    ["Materia", ["Correo"]],
-    ["Materia", ["Correo"]],
-]
+    "Institucional": {
+        "Vicerrectoria": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+        "Secretaria de Sede": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+        "Representacion Estudiantil": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+    },
 
-let sub_facultades = [
-    ["Facultad Minas", ["informa_fac_minas@unal.edu.co"]],
-    ["Facultad Ciencias", ["informa_fac_ciencias@unal.edu.co"]],
-    ["Facultad Artes y Arquitectura", ["informa_fac_arquitectura@unal.edu.co"]],
-    ["Facultad Humanas", ["informa_fac_ciencias_humanas_y_economicas@unal.edu.co"]],
-]
+    "Otros": {
+        "Post Master": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+        "Periodico UNAL": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+        "DINARA": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+        "Direccion Academica": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+        "Direccion de Relaciones Exteriores": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+        "Educacion Continua": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+        "Bienestar/Cultura": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+    },
 
-let sub_Otros = [
-    ["Post Master", ["postmaster_unal@unal.edu.co"]],
-    ["Periodico UNAL", ["infeducontinua@unal.edu.co"]],
-    ["DINARA", ["comdninfoa_nal@unal.edu.co"]],
-    ["Direccion Academica", ["informa_direccion_academica@unal.edu.co"]],
-    ["Direccion de Relaciones Exteriores", ["dre@unal.edu.co"]],
-    ["Educacion Continua", ["infeducontinua@unal.edu.co"]],
-    ["Bienestar/Cultura", ["logcultura_med@unal.edu.co", "ccultura_med@unal.edu.co"]],
-]
-
-let sub_I = [
-    ["", [""]],
-]
-
-let sub_Institucional = [
-    ["Vicerrectoria", ["informa_vicerrectoria@unal.edu.co"]],
-    ["Secretaria de Sede", ["informa_secretaria_sede@unal.edu.co"]],
-    ["Representacion Estudiantil", ["reestudia_med@unal.edu.co", "estudiantilca@unal.edu.co", "estudiantilcsu@unal.edu.co",]],
-]
-
-let Labels = {
-    "Institucional": sub_Institucional,
-    "Otros": sub_Otros,
-    "Informa Facultades": sub_facultades
+    "Informa Facultades":
+    {
+        "Facultad Minas": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+        "Facultad Ciencias": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+        "Facultad Artes y Arquitectura": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+        "Facultad Humanas": ["correo@correo.com", "correo2@correo.com"], //Sin correos por motivos de privacidad
+    },
 }
 
-function LabelId(Etiqueta) {
-    var request = Gmail.Users.Labels.list('me');
-    var name, id;
-    for (var l = 0; l < request.labels.length; l++) {
-        name = request.labels[l].name;
-        id = request.labels[l].id;
-        if (name === Etiqueta) {
-            return id
-        }
-    }
+const colors = {
+    "Institucional": {
+        "textColor": "#000000",
+        "backgroundColor": "#98d7e4",
+      },
+    "Otros": {
+        "textColor": "#ffffff",
+        "backgroundColor": "#fbd3e0",
+      },
+    "Informa Facultades": {
+        "textColor": "#000000",
+        "backgroundColor": "#e7e7e7",
+      },
 }
 
-function addFiltro(Correo, Id_Label) {
-    var filter = {
+function addFiltro(mail, label_id) {
+    Gmail.Users.Settings.Filters.create({
         criteria: {
-            from: Correo // Reemplaza con la dirección de correo deseada
+            from: mail // Reemplaza con la dirección de correo deseada
         },
         action: {
-            addLabelIds: [Id_Label], // Reemplaza con el ID de la etiqueta deseada
+            addLabelIds: [label_id],
             shouldArchive: false
         }
-    };
-    Gmail.Users.Settings.Filters.create(filter, "me");
-}
-
-function Prueba() {
-    console.log("tesssss")
+    }, "me");
 }
 
 function addLabels() {
-    for (let key of Object.keys(Labels)) {
-        GmailApp.createLabel(key)
+    for (const bigLabelname in allLabels) {
+        const subTags = allLabels[bigLabelname];
 
-        let Sub_Etiquetas = Labels[key]
+        const bigLabel = Gmail.Users.Labels.create({
+            "name": bigLabelname,
+            "color": colors[bigLabelname]
+        }, "me")
 
-        Sub_Etiquetas.forEach(subLabel => {
-            label_name = (key + "/" + subLabel[0])
-            GmailApp.createLabel(label_name)
+        for (const subEtiqueta in subTags) {
+            const subLabel = Gmail.Users.Labels.create({
+                "name": (bigLabelname + "/" + subEtiqueta),
+                "color": colors[bigLabelname]
+            }, "me")
 
-            let Id_Etiqueta = LabelId(label_name)
-            let Correos = subLabel[1]
-
-            Correos.forEach(correo => {
-                addFiltro(correo, Id_Etiqueta)
-            });
-
-            let Check = { "Nombre": label_name, "Correos": Correos }
-            addedLabels.push(Check)
-        });
-    }
-    console.log(addedLabels)
-}
-
-function getGmailFilters() {
-    var filters = Gmail.Users.Settings.Filters.list("me").filter;
-    for (var i = 0; i < filters.length; i++) {
-        var filter = filters[i];
-        Logger.log("Filtro " + (i + 1) + ":");
-        Logger.log("  ID: " + filter.id);
-        Logger.log("  From: " + filter.criteria.from);
-        Logger.log("  To: " + filter.criteria.to);
+            for (const mail of subTags[subEtiqueta]) {
+                addFiltro(mail, subLabel.id)
+            }
+        }
     }
 }
 
